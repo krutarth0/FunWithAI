4f84f5064cc95be23ef1781295376f066ac2fe31
Krutarth Patel <krutarthzero@hotmail.com>
2022-05-13 09:31:06 -0300

    no used vars

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useResponseDispatch, useResponses } from "../contexts/FunAiProvider";
 
 export default function Menu() {
